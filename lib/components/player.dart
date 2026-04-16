6482c2de1cf339157760b538aa3636436b06cce0
zopinos <zopi.tv@gmail.com>
2026-04-16 16:25:44 +0300

    Create a base for game

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,25 +1,37 @@
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
-import 'package:flame_forge2d/body_component.dart';
-import 'package:flame_forge2d/contact_callbacks.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:glare_game/constants/config.dart';
 import 'package:glare_game/game/glare_game.dart';
 
 class Player extends BodyComponent<GlareGame>
     with TapCallbacks, ContactCallbacks {
-  Player(Vector2 position)
-    : super(
-        bodyDef: BodyDef()
-          ..position = position
-          ..type = BodyType.dynamic
-          ..angularVelocity = 0.0,
-        fixtureDefs: [
-          FixtureDef(PolygonShape()..setAsBoxXY(2.5, 2.5))..restitution = 0.75,
-        ],
-      );
+  Player({required this.position}) : super();
+
+  @override
+  final Vector2 position;
+
+  @override
+  Body createBody() {
+    final bodyDef = BodyDef(
+      position: position,
+      type: BodyType.dynamic,
+      angularDamping: playerAngularDamping,
+      userData: this,
+    );
+
+    final shape = PolygonShape()..setAsBoxXY(2.5, 2.5);
+    final fixtureDef = FixtureDef(shape)..restitution = 0.75;
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
 
   void move(Vector2 impulse) {
+    if (impulse.length < 200.0) return;
+
+    impulse.clampLength(200.0, 8000.0);
+    print("impulse: $impulse, length: ${impulse.length}");
     body.linearVelocity = Vector2.zero();
+
     body.applyLinearImpulse(impulse);
   }
 }
--- lib/game/glare_game.dart
+++ lib/game/glare_game.dart
@@ -1,33 +1,42 @@
+import 'dart:math';
 import 'dart:ui';
 
-import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
-import 'package:flame/game.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:flame_forge2d/forge2d_game.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:glare_game/components/ball.dart';
-import 'package:glare_game/components/board.dart';
 import 'package:glare_game/components/game_bounds.dart';
+import 'package:glare_game/components/hittable.dart';
 import 'package:glare_game/components/player.dart';
-import 'package:glare_game/components/turret.dart';
 import 'package:glare_game/constants/config.dart';
 import 'package:glare_game/screens/result_screen.dart';
 import 'package:glare_game/services/level_service.dart';
 
 class GlareGame extends Forge2DGame with PanDetector {
   final levelService = Get.find<LevelService>();
+  final random = Random();
 
   var gameFinished = false;
 
   double get width => size.x;
   double get height => size.y;
 
-  final player = Player(Vector2(10.0, 10.0));
+  late Player player;
 
   Vector2? dragVelocity;
+
+  var timeLeft = 30.0;
+  var score = 0;
+
+  late double spawnFrequency;
+  var timeSinceLastSpawn = 0.0;
+
+  late Rect gameRect;
+  late double leftLocation;
+  late double rightLocation;
+  late double topLocation;
+  late double bottomLocation;
 
   GlareGame()
     : super(
@@ -45,20 +54,43 @@ class GlareGame extends Forge2DGame with PanDetector {
   Future<void> onLoad() async {
     await super.onLoad();
 
+    gameRect = camera.visibleWorldRect;
+    leftLocation = 0.0;
+    rightLocation = gameRect.width;
+    topLocation = 0.0;
+    bottomLocation = gameRect.height;
+
+    spawnFrequency = spawnFrequencies[levelService.currentLevel];
+
     camera.viewfinder.anchor = Anchor.topLeft;
 
     //final viewport = camera.viewport as FixedResolutionViewport;
 
     //viewport.add(TextComponent(text: "hello world", position: Vector2.zero()));
 
+    world.add(FinishLevelRectangle());
+
     world.add(GameBounds());
 
+    player = Player(position: gameRect.bottomRight.toVector2());
     world.add(player);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
+
+    timeLeft -= dt;
+
+    if (timeLeft <= 0 && !gameFinished) {
+      _finishGame();
+    }
+
+    timeSinceLastSpawn += dt;
+    if (timeSinceLastSpawn > spawnFrequency) {
+      _spawnHittable();
+      timeSinceLastSpawn = 0.0;
+    }
   }
 
   @override
@@ -74,22 +106,74 @@ class GlareGame extends Forge2DGame with PanDetector {
     player.move(dragVelocity!);
   }
 
-  finishGame() {
-    gameFinished = true;
-    levelService.completedLevels.add(levelService.currentLevel);
-    Get.offAll(() => ResultScreen(score: 10));
+  void _spawnHittable() {
+    final randomValue = random.nextDouble();
+
+    Vector2 spawnLocation;
+    Vector2 initialVelocity;
+
+    if (randomValue < 0.25) {
+      spawnLocation = Vector2(
+        leftLocation +
+            spawnOffset +
+            random.nextDouble() *
+                (rightLocation - spawnOffset * 2 - leftLocation),
+        topLocation,
+      );
+      initialVelocity = Vector2(0.0, 1.0) * hittableMaxSpeed;
+    } else if (randomValue < 0.50) {
+      spawnLocation = Vector2(
+        rightLocation,
+        topLocation +
+            spawnOffset +
+            random.nextDouble() *
+                (bottomLocation - spawnOffset * 2 - topLocation),
+      );
+      initialVelocity = Vector2(-1.0, 0.0) * hittableMaxSpeed;
+    } else if (randomValue < 0.75) {
+      spawnLocation = Vector2(
+        leftLocation +
+            spawnOffset +
+            random.nextDouble() *
+                (rightLocation - spawnOffset * 2 - leftLocation),
+        bottomLocation,
+      );
+      initialVelocity = Vector2(0.0, -1.0) * hittableMaxSpeed;
+    } else {
+      spawnLocation = Vector2(
+        leftLocation,
+        topLocation +
+            spawnOffset +
+            random.nextDouble() *
+                (bottomLocation - spawnOffset * 2 - topLocation),
+      );
+      initialVelocity = Vector2(1.0, 0.0) * hittableMaxSpeed;
+    }
+
+    print(
+      "randomValue: $randomValue, spawnLocation: $spawnLocation, initialVelocity: $initialVelocity",
+    );
+
+    world.add(Hittable(position: spawnLocation, velocity: initialVelocity));
   }
 
-  completeGame() {
+  void _finishGame() {
+    gameFinished = true;
+    levelService.completedLevels.add(levelService.currentLevel);
+    Get.offAll(() => ResultScreen(score: score));
+  }
+
+  void completeGame() {
     if (!gameFinished) {
-      finishGame();
+      _finishGame();
     }
   }
 }
 
-class TapRectangle extends RectangleComponent
+class FinishLevelRectangle extends RectangleComponent
     with HasGameRef<GlareGame>, TapCallbacks {
-  TapRectangle() : super(position: Vector2(100, 100), size: Vector2(50, 50));
+  FinishLevelRectangle()
+    : super(position: Vector2(1.0, 1.0), size: Vector2(2.0, 2.0));
 
   @override
   void onTapDown(TapDownEvent event) {
